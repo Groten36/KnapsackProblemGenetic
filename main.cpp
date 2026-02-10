c1b0aa2e93342d36a74c00bc109700087376febe
Groten <vaderhodor@gmail.com>
2026-02-10 13:35:04 +0100

    Check if repository cloned properly

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+std::vector<std::vector<bool>> generate_population(){
+    std::vector<std::vector<bool>> population;
+    for (auto i=0;i<10;i++){
+        population.emblace
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::bernoulli_distribution dist(0.5);
+
+}
+};
+}
+int main(){
+    std::vector<std::vector<bool> population;
+    int values[10]={5,	9,	1,	14,	8,	12,	5,	6,	3,	7};
+    int weights[10]={3,	12,	8,	11,	10,	7,	6,	2,	14,	2};
+return 0;
+}
